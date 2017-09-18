f3c8aeed2a0b955b66c8001a699b7f69c69d8b6f
jiaowoshabi <wenhaoz@usc.edu>
2017-09-17 17:18:20 -0700

    in ch12

---
+++ qa/tests-crosspage.js
@@ -0,0 +1,45 @@
+var Browser = require('zombie'),
+	assert = require('chai').assert;
+
+var browser;
+
+suite('Cross-Page Tests', function(){
+	
+	setup(function(){
+		browser = new Browser();
+	});
+
+	test('requesting a group rate quote from the hood river tour page' + 'should populate the referrer field', function(done){
+		var referrer = 'http://localhost:3000/tours/hood-river';
+		browser.visit(referrer, function(){
+			browser.clickLink('.requestGroupRate', function(){
+				console.log("1.referre is: " + browser.field('referrer').value);
+				assert(browser.field('referrer').value === '');
+				done();
+			});
+		});
+	});
+
+	test('requesting a group rate from the oregon coast tour page should' + 'populate the referrer field', function(done){
+		var referrer = 'http://localhost:3000/tours/oregon-coast';
+		browser.visit(referrer, function(){
+			browser.clickLink('.requestGroupRate', function(){
+				console.log("2.referre is: " + browser.field('referrer').value);
+				assert(browser.field('referrer').value === referrer);
+				done();
+			});
+		});
+	});
+
+
+	test('visiting the "request group rate" page directly should result' + 'in an empty referrer field', function(done){
+		var referrer = 'http://localhost:3000/tours/request-group-rate';
+		browser.visit(referrer, function(){
+			browser.clickLink('.requestGroupRate', function(){
+				console.log("3.referre is: " + browser.field('referrer').value);
+				assert(browser.field('referrer').value === '');
+				done();
+			});
+		});
+	});
+});
